682ded62aedff6222f85889da20f8cd072648786
PauloQuagliatto <pquagliatto.dev@hotmail.com>
2023-08-20 11:28:25 -0300

    added go to esquenta project

---
+++ esquenta/go/go.mod
@@ -0,0 +1,3 @@
+module github.com/PauloQuagliatto/imersao-fullcycle-14
+
+go 1.20
---
+++ esquenta/go/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+  "fmt" 
+  "time"
+)
+
+func worker(workerId int, data chan int) {
+  for x := range data {
+    fmt.Printf("Worker %d received %d\n", workerId, x)
+    time.Sleep(time.Second)
+  }
+}
+
+func main() {
+  channel := make(chan int)
+
+  for i := 0; i < 10000; i++ {
+    go worker(i, channel)
+  }
+
+  for i := 0; i < 10000; i++ {
+    channel <- i
+  }
+}
